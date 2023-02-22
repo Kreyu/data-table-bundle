6704352f63f8f5b65104742436613535848aa06e
Sebastian Wróblewski <kontakt@swroblewski.pl>
2023-02-23 00:01:59 +0100

    [Cleanup] Remove unnecessary "supports" method in FilterType

--- src/Filter/Type/FilterType.php
+++ src/Filter/Type/FilterType.php
@@ -15,11 +15,6 @@ use Symfony\Component\Translation\TranslatableMessage;
 
 final class FilterType implements FilterTypeInterface
 {
-    public function supports(ProxyQueryInterface $query): bool
-    {
-        return true;
-    }
-
     public function apply(ProxyQueryInterface $query, FilterData $data, FilterInterface $filter, array $options): void
     {
     }

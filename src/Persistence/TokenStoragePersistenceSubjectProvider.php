9488fb9414c20aa730547918ecde7ee73684476f
Sebastian Wróblewski <kontakt@swroblewski.pl>
2023-03-09 20:30:15 +0100

    [Feature] Improve token storage persistence subject provider by utilizing UserInterface->getUserIdentifier()

--- src/Persistence/TokenStoragePersistenceSubjectProvider.php
+++ src/Persistence/TokenStoragePersistenceSubjectProvider.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Kreyu\Bundle\DataTableBundle\Persistence;
 
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class TokenStoragePersistenceSubjectProvider implements PersistenceSubjectProviderInterface
 {
@@ -18,7 +19,17 @@ class TokenStoragePersistenceSubjectProvider implements PersistenceSubjectProvid
         $user = $this->tokenStorage->getToken()?->getUser();
 
         if ($user instanceof PersistenceSubjectInterface) {
-            return $user;
+            return new PersistenceSubjectAggregate(
+                $user->getDataTablePersistenceIdentifier(),
+                $user,
+            );
+        }
+
+        if ($user instanceof UserInterface) {
+            return new PersistenceSubjectAggregate(
+                $user->getUserIdentifier(),
+                $user,
+            );
         }
 
         throw PersistenceSubjectNotFoundException::createForProvider($this);

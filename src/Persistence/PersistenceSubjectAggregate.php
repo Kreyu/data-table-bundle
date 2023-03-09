5cdc3b16af693552ce38a71dc30c216b5529b3d5
Sebastian Wróblewski <kontakt@swroblewski.pl>
2023-03-09 20:30:15 +0100

    [Feature] Improve token storage persistence subject provider by utilizing UserInterface->getUserIdentifier()

---
+++ src/Persistence/PersistenceSubjectAggregate.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kreyu\Bundle\DataTableBundle\Persistence;
+
+/**
+ * Persistence subject that holds a reference to an original subject.
+ */
+class PersistenceSubjectAggregate implements PersistenceSubjectInterface
+{
+    public function __construct(
+        private string $identifier,
+        private mixed $subject,
+    ) {
+    }
+
+    public function getDataTablePersistenceIdentifier(): string
+    {
+        return $this->identifier;
+    }
+
+    public function getSubject(): mixed
+    {
+        return $this->subject;
+    }
+}

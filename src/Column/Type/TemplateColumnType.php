9a442a5467fb8709a2eadb21e6971f5ba5460149
Sebastian Wróblewski <kontakt@swroblewski.pl>
2023-06-12 15:25:51 +0200

    bugfix: missing template column type variables

--- src/Column/Type/TemplateColumnType.php
+++ src/Column/Type/TemplateColumnType.php
@@ -4,10 +4,20 @@ declare(strict_types=1);
 
 namespace Kreyu\Bundle\DataTableBundle\Column\Type;
 
+use Kreyu\Bundle\DataTableBundle\Column\ColumnInterface;
+use Kreyu\Bundle\DataTableBundle\Column\ColumnValueView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TemplateColumnType extends AbstractColumnType
 {
+    public function buildValueView(ColumnValueView $view, ColumnInterface $column, array $options): void
+    {
+        $view->vars = array_merge($view->vars, [
+            'template_path' => $options['template_path'],
+            'template_vars' => $options['template_vars'],
+        ]);
+    }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver

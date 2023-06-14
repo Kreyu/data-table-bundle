a6e19c8a85416d5071061e4870db1dc5dee45f69
Sebastian Wróblewski <kontakt@swroblewski.pl>
2023-06-14 17:28:17 +0200

    feat: change DataTableFactoryInterface $query argument type to "mixed"

--- src/DataTableFactoryInterface.php
+++ src/DataTableFactoryInterface.php
@@ -4,15 +4,13 @@ declare(strict_types=1);
 
 namespace Kreyu\Bundle\DataTableBundle;
 
-use Kreyu\Bundle\DataTableBundle\Query\ProxyQueryInterface;
-
 interface DataTableFactoryInterface
 {
-    public function create(string $type, ?ProxyQueryInterface $query = null, array $options = []): DataTableInterface;
+    public function create(string $type, mixed $query = null, array $options = []): DataTableInterface;
 
-    public function createNamed(string $name, string $type, ?ProxyQueryInterface $query = null, array $options = []): DataTableInterface;
+    public function createNamed(string $name, string $type, mixed $query = null, array $options = []): DataTableInterface;
 
-    public function createBuilder(string $type, ?ProxyQueryInterface $query = null, array $options = []): DataTableBuilderInterface;
+    public function createBuilder(string $type, mixed $query = null, array $options = []): DataTableBuilderInterface;
 
-    public function createNamedBuilder(string $name, string $type, ?ProxyQueryInterface $query = null, array $options = []): DataTableBuilderInterface;
+    public function createNamedBuilder(string $name, string $type, mixed $query = null, array $options = []): DataTableBuilderInterface;
 }

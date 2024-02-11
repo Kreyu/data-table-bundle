42d843e3e2a3c7d07842e3e26483b49f2b0071a0
Sebastian Wróblewski <kontakt@swroblewski.pl>
2024-02-11 22:29:10 +0100

    feat: remove no longer used DataTablePass compiler pass

--- src/KreyuDataTableBundle.php
+++ src/KreyuDataTableBundle.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Kreyu\Bundle\DataTableBundle;
 
-use Kreyu\Bundle\DataTableBundle\DependencyInjection\DataTablePass;
 use Kreyu\Bundle\DataTableBundle\DependencyInjection\DefaultConfigurationPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
@@ -13,7 +12,6 @@ class KreyuDataTableBundle extends Bundle
 {
     public function build(ContainerBuilder $container): void
     {
-        $container->addCompilerPass(new DataTablePass());
         $container->addCompilerPass(new DefaultConfigurationPass());
     }
 }

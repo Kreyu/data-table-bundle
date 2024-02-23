33c42434d7fefb676be88e1292a8fbdb71738110
Sebastian Wróblewski <kontakt@swroblewski.pl>
2024-02-23 10:40:36 +0100

    fix: validate export filename to prevent exceptions

--- src/Exporter/Form/Type/ExportDataType.php
+++ src/Exporter/Form/Type/ExportDataType.php
@@ -12,13 +12,30 @@ use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class ExportDataType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('filename', TextType::class)
+            ->add('filename', TextType::class, [
+                'constraints' => [
+                    new Constraints\NotBlank(),
+                    new Constraints\Callback(function (?string $value, ExecutionContextInterface $context) {
+                        if (null === $value) {
+                            return;
+                        }
+
+                        if (str_contains($value, '/') || str_contains($value, '\\')) {
+                            $context->buildViolation('The filename cannot contain following characters: "\/"')
+                                ->addViolation()
+                            ;
+                        }
+                    }),
+                ],
+            ])
             ->add('exporter', ChoiceType::class, [
                 'choices' => array_flip(array_map(
                     fn (ExporterInterface $exporter) => $exporter->getConfig()->getOption('label') ?? $exporter->getName(),

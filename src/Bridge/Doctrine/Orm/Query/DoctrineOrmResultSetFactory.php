3ecaa0bf8cd11f35bee5ea25b2c63dd43ad380d0
Sebastian Wróblewski <kontakt@swroblewski.pl>
2024-07-17 12:49:59 +0200

    Disable entity manager clearing in Doctrine ORM result set factory (temporarily, until better, non-breaking solution is found)

--- src/Bridge/Doctrine/Orm/Query/DoctrineOrmResultSetFactory.php
+++ src/Bridge/Doctrine/Orm/Query/DoctrineOrmResultSetFactory.php
@@ -59,7 +59,10 @@ class DoctrineOrmResultSetFactory implements DoctrineOrmResultSetFactoryInterfac
 
             $firstResult += $itemCount;
 
-            $paginator->getQuery()->getEntityManager()->clear();
+            // TODO: Make clearing configurable and optional.
+            //       The code below alone breaks the UnitOfWork when rendering 
+            //       data tables with previously loaded entities on the same page.
+            // $paginator->getQuery()->getEntityManager()->clear();
         }
     }
 }

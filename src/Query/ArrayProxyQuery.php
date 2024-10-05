998dadaf1a258399d229dc9081ae8c131578563e
Sebastian Wróblewski <kontakt@swroblewski.pl>
2024-10-05 13:28:31 +0200

    Array source (#134)

---
+++ src/Query/ArrayProxyQuery.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kreyu\Bundle\DataTableBundle\Query;
+
+use Kreyu\Bundle\DataTableBundle\Pagination\PaginationData;
+use Kreyu\Bundle\DataTableBundle\Sorting\SortingData;
+use Symfony\Component\PropertyAccess\PropertyAccess;
+
+class ArrayProxyQuery implements ProxyQueryInterface
+{
+    private ?array $originalData = null;
+    private ?array $sortedData = null;
+
+    public function __construct(
+        private array $data,
+        private ?int $totalItemCount = null,
+    ) {
+        $this->originalData = $this->data;
+        $this->sortedData = $this->data;
+        $this->totalItemCount ??= count($this->data);
+    }
+
+    public function sort(SortingData $sortingData): void
+    {
+        $propertyAccessor = PropertyAccess::createPropertyAccessor();
+
+        $this->originalData ??= $this->data;
+
+        $this->data = $this->originalData;
+
+        usort($this->data, function ($a, $b) use ($sortingData, $propertyAccessor) {
+            foreach ($sortingData->getColumns() as $sortingColumnData) {
+                $propertyPath = $sortingColumnData->getPropertyPath();
+                $direction = $sortingColumnData->getDirection();
+
+                $valueA = $propertyAccessor->getValue($a, $propertyPath);
+                $valueB = $propertyAccessor->getValue($b, $propertyPath);
+
+                if ($valueA < $valueB) {
+                    return 'asc' === $direction ? -1 : 1;
+                } elseif ($valueA > $valueB) {
+                    return 'asc' === $direction ? 1 : -1;
+                }
+            }
+
+            return 0;
+        });
+
+        $this->sortedData = $this->data;
+    }
+
+    public function paginate(PaginationData $paginationData): void
+    {
+        $this->data = array_slice(
+            $this->sortedData ?? $this->originalData,
+            $paginationData->getOffset(),
+            $paginationData->getPerPage(),
+        );
+    }
+
+    public function getResult(): ResultSetInterface
+    {
+        return new ResultSet(
+            iterator: new \ArrayIterator($this->data),
+            currentPageItemCount: count($this->data),
+            totalItemCount: $this->totalItemCount,
+        );
+    }
+}
---
+++ tests/Unit/Query/ArrayProxyQueryFactoryTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kreyu\Bundle\DataTableBundle\Tests\Unit\Query;
+
+use Kreyu\Bundle\DataTableBundle\Query\ArrayProxyQuery;
+use Kreyu\Bundle\DataTableBundle\Query\ArrayProxyQueryFactory;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+class ArrayProxyQueryFactoryTest extends TestCase
+{
+    private ArrayProxyQueryFactory $factory;
+
+    protected function setUp(): void
+    {
+        $this->factory = new ArrayProxyQueryFactory();
+    }
+
+    public function testCreate()
+    {
+        $this->assertInstanceOf(ArrayProxyQuery::class, $this->factory->create([]));
+    }
+
+    #[DataProvider('provideSupportsCases')]
+    public function testSupports(mixed $data, bool $expected)
+    {
+        $this->assertEquals($expected, $this->factory->supports($data));
+    }
+
+    public static function provideSupportsCases(): iterable
+    {
+        yield 'array' => [[], true];
+        yield 'string' => ['', false];
+        yield 'integer' => [123, false];
+        yield 'bool' => [true, false];
+        yield 'null' => [null, false];
+        yield 'object' => [new \stdClass(), false];
+    }
+}

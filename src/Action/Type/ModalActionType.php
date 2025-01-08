0a6ccbb2a2c25275fc35316e4194fdb268936764
Alexandre Castelain <alexandre.castelain@fulgens.be>
2025-01-08 14:51:38 +0100

    Add a modal action whose content is loaded via an HTTP call.

---
+++ src/Action/Type/ModalActionType.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Kreyu\Bundle\DataTableBundle\Action\Type;
+
+use Kreyu\Bundle\DataTableBundle\Action\ActionInterface;
+use Kreyu\Bundle\DataTableBundle\Action\ActionView;
+use Kreyu\Bundle\DataTableBundle\Column\ColumnValueView;
+use Kreyu\Bundle\DataTableBundle\Exception\LogicException;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\RouterInterface;
+
+final class ModalActionType extends AbstractActionType
+{
+    public function __construct(
+        private readonly RouterInterface $router,
+    ) {
+    }
+
+    public function buildView(ActionView $view, ActionInterface $action, array $options): void
+    {
+        if (null === $options['href'] && null === $options['route']) {
+            throw new LogicException('No "route" or "href" provided.');
+        }
+
+        if ($view->parent instanceof ColumnValueView) {
+            $value = $view->parent->value;
+
+            foreach (['href', 'route', 'route_params'] as $optionName) {
+                if (isset($options[$optionName]) && is_callable($options[$optionName])) {
+                    $options[$optionName] = $options[$optionName]($value);
+                }
+            }
+        } else {
+            foreach (['href', 'route', 'route_params'] as $optionName) {
+                if (isset($options[$optionName]) && is_callable($options[$optionName])) {
+                    throw new LogicException(sprintf('Callable used for option "%s", but it\'s only available for RowActions.', $optionName));
+                }
+            }
+        }
+
+        $href = $options['href'] ?? $this->router->generate($options['route'], $options['route_params']);
+
+        $view->vars = array_replace($view->vars, [
+            'href' => $href,
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver
+            ->define('route')
+            ->allowedTypes('string', 'callable')
+        ;
+
+        $resolver
+            ->define('route_params')
+            ->allowedTypes('array', 'callable')
+            ->default([])
+        ;
+
+        $resolver
+            ->define('href')
+            ->allowedTypes('string', 'callable')
+        ;
+    }
+}
--- src/Resources/config/actions.php
+++ src/Resources/config/actions.php
@@ -10,6 +10,7 @@ use Kreyu\Bundle\DataTableBundle\Action\Type\ActionType;
 use Kreyu\Bundle\DataTableBundle\Action\Type\ButtonActionType;
 use Kreyu\Bundle\DataTableBundle\Action\Type\FormActionType;
 use Kreyu\Bundle\DataTableBundle\Action\Type\LinkActionType;
+use Kreyu\Bundle\DataTableBundle\Action\Type\ModalActionType;
 use Kreyu\Bundle\DataTableBundle\Action\Type\ResolvedActionTypeFactory;
 use Kreyu\Bundle\DataTableBundle\Action\Type\ResolvedActionTypeFactoryInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -59,5 +60,11 @@ return static function (ContainerConfigurator $configurator) {
     $services
         ->set('kreyu_data_table.action.type.form', FormActionType::class)
         ->tag('kreyu_data_table.action.type')
+    ;
+
+    $services
+        ->set('kreyu_data_table.action.type.modal', ModalActionType::class)
+        ->tag('kreyu_data_table.action.type')
+        ->args([service('router')])
     ;
 };

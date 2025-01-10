37eddaf08935c2345e6e39bb2003e50a0e0ed595
Alexandre Castelain <alexandre.castelain@fulgens.be>
2025-01-10 16:29:34 +0100

    Revert the DataTableBuilderInterface.php

--- src/DataTableBuilderInterface.php
+++ src/DataTableBuilderInterface.php
@@ -144,6 +144,25 @@ interface DataTableBuilderInterface extends DataTableConfigBuilderInterface
      */
     public function getRowActions(): array;
 
+    /**
+     * @throws InvalidArgumentException if row action of given name does not exist
+     */
+    public function getRowAction(string $name): ActionBuilderInterface;
+
+    public function hasRowAction(string $name): bool;
+
+    /**
+     * @param class-string<ActionTypeInterface>|null $type
+     */
+    public function createRowAction(string $name, ?string $type = null, array $options = []): ActionBuilderInterface;
+
+    /**
+     * @param class-string<ActionTypeInterface>|null $type
+     */
+    public function addRowAction(ActionBuilderInterface|string $action, ?string $type = null, array $options = []): static;
+
+    public function removeRowAction(string $name): static;
+
     public function isAutoAddingActionsColumn(): bool;
 
     public function setAutoAddingActionsColumn(bool $autoAddingActionsColumn): static;

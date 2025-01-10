091bd07c9401d66a1abf501ab72f3b98f595a240
alexandre-castelain <alexandre.castelain@fulgens.be>
2025-01-10 15:46:37 +0100

    Add the "supported_operators" as option to be overridden in Doctrine ORM filter types (#169)

--- src/Bridge/Doctrine/Orm/Filter/Type/NumericFilterType.php
+++ src/Bridge/Doctrine/Orm/Filter/Type/NumericFilterType.php
@@ -4,29 +4,24 @@ declare(strict_types=1);
 
 namespace Kreyu\Bundle\DataTableBundle\Bridge\Doctrine\Orm\Filter\Type;
 
-use Kreyu\Bundle\DataTableBundle\Filter\FilterBuilderInterface;
 use Kreyu\Bundle\DataTableBundle\Filter\Operator;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class NumericFilterType extends AbstractDoctrineOrmFilterType
 {
-    public function buildFilter(FilterBuilderInterface $builder, array $options): void
-    {
-        $builder->setSupportedOperators([
-            Operator::Equals,
-            Operator::NotEquals,
-            Operator::Contains,
-            Operator::NotContains,
-            Operator::StartsWith,
-            Operator::EndsWith,
-        ]);
-    }
-
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'form_type' => NumberType::class,
+            'supported_operators' => [
+                Operator::Equals,
+                Operator::NotEquals,
+                Operator::Contains,
+                Operator::NotContains,
+                Operator::StartsWith,
+                Operator::EndsWith,
+            ],
         ]);
     }
 }
